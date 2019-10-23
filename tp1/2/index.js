ffdc8fa3ddf4f36367b97d9d96a65813743280a5
Celestarien <43401854+Celestarien@users.noreply.github.com>
2019-10-23 16:08:29 +0200

    Update index.js

--- tp1/2/index.js
+++ tp1/2/index.js
@@ -7,7 +7,18 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
-
+     let cartons = input[0]
+    let poid = 0
+    let aller_retour_compteur = 1
+    for (let index = 1; index < input.length; index++) {
+      const element = input[index];
+      if ((poid + element) <= 100) {
+        poid += element
+      } else {
+        aller_retour_compteur += 1
+        poid = element
+      }
+    }
     // AND HERE
   },
   verify: function (dataset, output) {
